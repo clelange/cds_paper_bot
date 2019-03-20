87c11dd70f21d1fc80667fb716abfd9919ea75ea
Clemens Lange <clemens.lange@cern.ch>
2019-03-20 01:34:29 +0100

    CI with push rights

--- .gitlab/before_script.sh
+++ .gitlab/before_script.sh
@@ -12,21 +12,20 @@ CONSUMER_SECRET = ${CONSUMER_SECRET}
 ACCESS_TOKEN = ${ACCESS_TOKEN}
 ACCESS_TOKEN_SECRET = ${ACCESS_TOKEN_SECRET}
 EOF
+# Create the SSH directory and give it the right permissions
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
+eval "$(ssh-agent -s)"
+ssh-add <(echo "$GIT_SSH_PRIV_KEY")
+ssh-keyscan gitlab.cern.ch > ~/.ssh/known_hosts
 # set +x
-# # Create the SSH directory and give it the right permissions
-# mkdir -p ~/.ssh
-# chmod 700 ~/.ssh
-# # Adjust SSH config to allow kerberos authentication
-# cat .gitlab/ssh_config > ~/.ssh/config
-# # Set git user name and email
-# set +x
-# echo "${SVNPASS}" | kinit "${SVNUSER}@CERN.CH" > /dev/null
+
 set -x
+# # Set git user name and email
 git config --global user.email "${GITMAIL}"
 git config --global user.name "${GITNAME}"
 git config --global http.postBuffer 524288000
 git config --global https.postBuffer 524288000
-
 
 set +x
 echo "auth.ini created for ${EXPERIMENT}"
--- .gitlab/script.sh
+++ .gitlab/script.sh
@@ -7,8 +7,7 @@ python cds_paper_bot.py --dry -e "${EXPERIMENT}"
 if git status --porcelain; then
     git add ./*_FEED.txt
     git commit -m "update tweeted analyses"
-    git remote add kerberos "${REMOTE_GIT_REPO}"
-    git push kerberos HEAD
+    git push origin HEAD
 else
     echo "No changes found."
 fi
