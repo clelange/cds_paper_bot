0f9bfab770e2d8eda17daae5530771cbb19ec92c
Sascha Mehlhase <sascha.mehlhase@cern.ch>
2019-06-07 09:50:38 +0000

    allow for 10 pubs to be tweeted

--- .gitlab/script.sh
+++ .gitlab/script.sh
@@ -3,7 +3,7 @@
 set -e
 # echo on
 set -x
-python cds_paper_bot.py -m 1 -e "${EXPERIMENT}"
+python cds_paper_bot.py -m 10 -e "${EXPERIMENT}"
 if [[ -n $(git status -s) ]]; then
     git checkout master
     git add ./*_FEED.txt
