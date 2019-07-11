40728a87b65bd930cc6ca7675cd20cafec9439b0
Clemens Lange <clemens.lange@cern.ch>
2019-07-11 10:50:54 +0200

    remove commented line

--- .gitlab/script.sh
+++ .gitlab/script.sh
@@ -3,7 +3,6 @@
 set -e
 # echo on
 set -x
-# python cds_paper_bot.py -a "ATLAS-CONF-2019-021" -d -e "${EXPERIMENT}"
 python cds_paper_bot.py -m 1 -e "${EXPERIMENT}" --arXiv
 if [[ -n $(git status -s) ]]; then
     git checkout master
